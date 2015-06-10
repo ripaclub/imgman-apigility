5e6ed8796039da31bdd3a424aa3aba62ada5165a
Leonardo Di Donato <leodidonato@gmail.com>
2015-06-10 16:19:51 +0200

    Reorganisation

--- Module.php
+++ Module.php
@@ -1,12 +1,16 @@
 <?php
 namespace ImgMan\Apigility;
 
+use Solo\Api\Cors\CorsRouteListener;
+use Solo\Api\Error\UnhandledExceptionListener;
 use Solo\Api\MvcAuth\Authentication\IdentityPostAuthenticationListener;
 use Solo\Api\MvcAuth\Authentication\UnauthenticatedListener;
 use Solo\Api\MvcAuth\Authorization\DefaultAuthorizationListener;
 use Solo\Api\MvcAuth\Authorization\UnauthorizedListener;
 use Zend\Console\Console;
 use Zend\Http\Header\Accept\FieldValuePart\LanguageFieldValuePart;
+use Zend\Http\Header\AcceptLanguage;
+use Zend\Http\Response;
 use Zend\ModuleManager\Feature\HydratorProviderInterface;
 use Zend\ModuleManager\Feature\InputFilterProviderInterface;
 use Zend\Mvc\MvcEvent;
@@ -15,10 +19,6 @@ use Zend\View\Model\JsonModel;
 use ZF\Apigility\Provider\ApigilityProviderInterface;
 use ZF\Hal\View\HalJsonModel;
 use ZF\MvcAuth\MvcAuthEvent;
-use Zend\Http\Header\AcceptLanguage;
-use Solo\Api\Error\UnhandledExceptionListener;
-use Zend\Http\Response;
-use Solo\Api\Cors\CorsRouteListener;
 
 /**
  * Class Module
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# imgman-apigility
+ImgMan connected resource for Apigility
+---------------------------------------
+
+**WIP**
+
+---
+
+[![Analytics](https://ga-beacon.appspot.com/UA-49655829-1/ripaclub/zf-imgman-apigility)](https://github.com/igrigorik/ga-beacon)

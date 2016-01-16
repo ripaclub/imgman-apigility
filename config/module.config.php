c777648758b9d370f9a7ba42fd411ffae8d2c660
visa4 <antonino.visalli@gmail.com>
2016-01-16 18:51:54 +0100

    Fix module config

--- config/module.config.php
+++ config/module.config.php
@@ -1,9 +1,17 @@
 <?php
-
 return [
     'service_manager' => [
         'abstract_factories' => [
             'ImgMan\Apigility\ImgManConnectedResourceAbstractFactory',
+            'ImgMan\Service\ImageServiceAbstractFactory',
+            'ImgMan\Storage\Adapter\Mongo\MongoDbAbstractServiceFactory',
+            'ImgMan\Storage\Adapter\Mongo\MongoAdapterAbstractServiceFactory'
+        ],
+        'factories' => [
+            'ImgMan\PluginManager' => 'ImgMan\Operation\HelperPluginManagerFactory',
+        ],
+        'invokables' => [
+            'ImgMan\Adapter\Imagick'  => 'ImgMan\Core\Adapter\ImagickAdapter',
         ]
     ]
 ];
